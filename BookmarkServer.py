b03ef4a500ba5f1307b048a206c366914594d8c6
Rajat Nagpure <rajatnagpureiiest@gmail.com>
2020-03-24 16:51:20 +0530

    third commit

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -150,6 +150,6 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             self.wfile.write("Unknown site asked")
 
 if __name__ == '__main__':
-    server_address = ('', 8000)
+    server_address = ('', 8080)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
