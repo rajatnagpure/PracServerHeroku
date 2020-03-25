5d480b33e5a9f0efc30505231efe866d4a6c0786
Rajat Nagpure <rajatnagpureiiest@gmail.com>
2020-03-25 13:02:13 +0530

    fifth commit

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -44,6 +44,8 @@
 import http.server
 import requests
 import os
+import threading
+from socketserver import ThreadingMixIn
 from urllib.parse import unquote, parse_qs
 
 memory = {}
@@ -150,8 +152,11 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write("Unknown site asked")
 
+class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
+    "This is an HTTPServer that supports thread-based concurrency."
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
     server_address = ('', port)
-    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd = ThreadHTTPServer(server_address, Shortener)
     httpd.serve_forever()
